03b83cb5f6c336494315621a7229c885d2acb8ca
Yasmany Cubela Medina <yasmanycm@gmail.com>
2024-03-14 12:53:23 -0500

    feat: rename debug tool to logger

--- logger/dagger/src/main.py
+++ logger/dagger/src/main.py
@@ -2,11 +2,11 @@ from dagger import dag, function, object_type
 
 
 @object_type
-class Debug:
-    """Debug Artifact."""
+class Logger:
+    """Logger Artifact."""
 
     @function
-    async def debug(self, message: str) -> str:
+    async def log(self, message: str) -> str:
         """Debug the message."""
 
         return await dag.container().from_(address="alpine").with_exec(["echo", message]).stdout()
